4cc7c6001d335b6c1ddfb04801e97053ad3d9d92
jared <dicarloj@mit.edu>
2019-07-01 12:32:29 -0400

    add old convex mpc

--- user/MIT_Controller/Controllers/convexMPC/convexMPC_interface.h
+++ user/MIT_Controller/Controllers/convexMPC/convexMPC_interface.h
@@ -12,27 +12,30 @@
 
 struct problem_setup
 {
-    float dt;
-    float mu;
-    float f_max;
-    int horizon;
+  float dt;
+  float mu;
+  float f_max;
+  int horizon;
 };
 
 struct update_data_t
 {
-    float p[3];
-    float v[3];
-    float q[4];
-    float w[3];
-    float r[12];
-    float yaw;
-    float weights[12];
-    float traj[12*K_MAX_GAIT_SEGMENTS];
-    float alpha;
-    unsigned char gait[K_MAX_GAIT_SEGMENTS];
+  float p[3];
+  float v[3];
+  float q[4];
+  float w[3];
+  float r[12];
+  float yaw;
+  float weights[12];
+  float traj[12*K_MAX_GAIT_SEGMENTS];
+  float alpha;
+  unsigned char gait[K_MAX_GAIT_SEGMENTS];
 };
 
 EXTERNC void setup_problem(double dt, int horizon, double mu, double f_max);
 EXTERNC void update_problem_data(double* p, double* v, double* q, double* w, double* r, double yaw, double* weights, double* state_trajectory, double alpha, int* gait);
 EXTERNC double get_solution(int index);
+EXTERNC void update_problem_data_floats(float* p, float* v, float* q, float* w,
+                                        float* r, float yaw, float* weights,
+                                        float* state_trajectory, float alpha, int* gait);
 #endif
